4babe839068a0c6f210522476a07b85d7acf3bc4
halah03 <mud2200@gmail.com>
2021-12-20 22:20:47 +0300

    Day2

---
+++ Day2/UserController.java
@@ -0,0 +1,43 @@
+package com.example.moeenms.controller;
+
+
+
+import com.example.moeenms.model.Entities.User;
+import com.example.moeenms.model.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/moeen")
+public class UserController<userService> {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+
+    @GetMapping(path = "{userId}")
+    public Optional<User> getUser(@PathVariable (name = "userId") Integer userId) {
+        return userService.getUser(userId);
+    }
+
+    @PostMapping(path= "add")
+    public void registerNewUser(@RequestBody User user){
+        userService.addNewUser(user);
+    }
+    @DeleteMapping(path = "delete/{userId}")
+    public void deleteUser(@PathVariable ("userId") Integer userId){
+        userService.deleteUser(userId);
+    }
+}
